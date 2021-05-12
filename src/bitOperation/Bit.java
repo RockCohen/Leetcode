7058ce0844786e8749fd0acf338b83f1a28d3efd
RockCohen <xluo0616@gmail.com>
2021-05-12 10:15:38 +0800

    位运算+1，数组的前缀异或

--- src/bitOperation/Bit.java
+++ src/bitOperation/Bit.java
@@ -27,4 +27,43 @@ public class Bit {
         for (int num : nums) res ^= num;
         return res;
     }
+
+    /**
+     * 题目：子数组异或查询
+     * @param arr
+     * @param queries
+     * @return
+     * @see https://leetcode-cn.com/problems/xor-queries-of-a-subarray/
+     *
+     * 题解：
+     * 1. 暴力模拟（显然是一种可能超时的算法）
+     * 2. 存储部分结果（显然存在已经计算的结果作为后续计算结果的部分，
+     *    应该利用这部分计算过的结果来减少计算重复性）。
+     *    该题目需要了解一个叫做数组前缀异或。
+     *    我们定义一个长度为n+1个元素的前缀异或数组xors。
+     *    令 xors[0]=0，对于0≤i<n，xors[i+1]=xors[i]⊕arr[i]，其中 ⊕ 是异或运算符。
+     *    xors[i]=arr[0]⊕…⊕arr[i−1]。
+     *    有了上述的数组的前缀异或，我们就可以计算任意子区间的异或结果。我们直到有异或运算的特点：
+     *    0^0=0;
+     *    1^1=0;
+     *    0^1=1;
+     *    1^0=1;
+     *    于是基于上述的特点我们可以利用数组的前缀异或来计算任意子数组的异或结果。
+     *    存在数组arr[n]我们需要计算[left,right]区间的异或结果。
+     *    显然：Xor[left,right]=xors[left]^xors[right+1].
+     *    没啥可说的，异或运算，yyds.
+     */
+    public int[] xorQueries(int[] arr, int[][] queries) {
+        int n = arr.length;
+        int[] xors = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            xors[i + 1] = xors[i] ^ arr[i];
+        }
+        int m = queries.length;
+        int[] ans = new int[m];
+        for (int i = 0; i < m; i++) {
+            ans[i] = xors[queries[i][0]] ^ xors[queries[i][1] + 1];
+        }
+        return ans;
+    }
 }
--- src/tree/TreeOperating.java
+++ src/tree/TreeOperating.java
@@ -682,6 +682,39 @@ public class TreeOperating {
         }
         else return root.val+sumSearch(root.left,low,high)+sumSearch(root.right,low,high);
     }
+
+    /**
+     * 题目：叶子相似的树
+     * @param root1
+     * @param root2
+     * @return
+     * @see https://leetcode-cn.com/problems/leaf-similar-trees/
+     * 题解思路：深度优先搜索
+     * 通过列表直接存储叶子节点，比对列表元素是否一致。
+     */
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> seq1 = new ArrayList<Integer>();
+        if (root1 != null) {
+            dfs(root1, seq1);
+        }
+        List<Integer> seq2 = new ArrayList<Integer>();
+        if (root2 != null) {
+            dfs(root2, seq2);
+        }
+        return seq1.equals(seq2);
+    }
+    public void dfs(TreeNode node, List<Integer> seq) {
+        if (node.left == null && node.right == null) {
+            seq.add(node.val);
+        } else {
+            if (node.left != null) {
+                dfs(node.left, seq);
+            }
+            if (node.right != null) {
+                dfs(node.right, seq);
+            }
+        }
+    }
     public static void main(String[] args){
         TreeNode root=new TreeNode(90,new TreeNode(69,new TreeNode(49,null,new TreeNode(52)),new TreeNode(89)),null);
         int res=new TreeOperating().minDiffInBST(root);
